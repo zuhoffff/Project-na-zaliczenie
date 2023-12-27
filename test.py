3615c2fbb85d52eec31c3da720e3ee1a0c283934
Nick <snicky5746@gmail.com>
2023-12-27 10:19:29 +0100

    set_axis implementation with oop

--- test.py
+++ test.py
@@ -4,59 +4,68 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# Function to generate new points within a distance of 1 from the previous point
-def generate_next_point(x, y):
-    angle = np.random.uniform(0, 2 * np.pi)
-    distance = np.random.uniform(0, 1)
-    new_x = x + distance * np.cos(angle)
-    new_y = y + distance * np.sin(angle)
-    return new_x, new_y
+class Mice:
+    def __init__(self, mice_num, num_frames):
+        self.mice_num = mice_num
+        self.num_frames = num_frames
+        self.fig, self.ax = plt.subplots()
+        self.lines = [self.ax.plot([], [],color='blue', label=f'mouse {i+1}')[0] for i in range(mice_num)]
+        #self.line_color = 'blue'
+        self.mice_pos = [self.generate_points(np.random.uniform(-5, 5), np.random.uniform(-5, 5)) for _ in range(mice_num)]
+        self.init()
 
-# Generate points with random starting coordinates
-def generate_points(num_frames, start_x, start_y):
-    x_data = np.zeros(num_frames)
-    y_data = np.zeros(num_frames)
-    x_data[0] = start_x
-    y_data[0] = start_y
-    for i in range(1, num_frames):
-        x_data[i], y_data[i] = generate_next_point(x_data[i - 1], y_data[i - 1])
-    return x_data, y_data
+    def generate_next_point(self, x, y):
+        angle = np.random.uniform(0, 2 * np.pi)
+        distance = np.random.uniform(0, 1)
+        new_x = x + distance * np.cos(angle)
+        new_y = y + distance * np.sin(angle)
+        return new_x, new_y
 
-# Number of mice
-mice_num = 2
-num_frames = 100
+    def generate_points(self, start_x, start_y):
+        x_data = np.zeros(self.num_frames)
+        y_data = np.zeros(self.num_frames)
+        x_data[0] = start_x
+        y_data[0] = start_y
+        for i in range(1, self.num_frames):
+            x_data[i], y_data[i] = self.generate_next_point(x_data[i - 1], y_data[i - 1])
+        return x_data, y_data
 
-# Set up the figure and axis
-fig, ax = plt.subplots()
+    def init(self):
+        for line in self.lines:
+            line.set_data([], [])
+        return tuple(self.lines)
+        #self.ax.legend()
 
-# Initialize arrays with random starting coordinates
-mice_pos = [generate_points(num_frames, np.random.uniform(-5, 5), np.random.uniform(-5, 5)) for _ in range(mice_num)]
-lines = [ax.plot([], [],color = 'blue', label=f'mouse {i+1}')[0] for i in range(mice_num)]
+    def update(self, frame):
+        for i in range(self.mice_num):
+            self.lines[i].set_data(self.mice_pos[i][0][:frame + 1], self.mice_pos[i][1][:frame + 1])
+        return tuple(self.lines)
 
-# Function to initialize the plot
-def init():
-    for line in lines:
-        line.set_data([], [])
-    return tuple(lines)
+    def set_axis(self):
+        x_min = self.mice_pos[0][0]
+        x_max = self.mice_pos[0][0]
+        y_min = self.mice_pos[0][1]
+        y_max = self.mice_pos[0][1]
+        for i in range (self.mice_num):
+            if self.mice_pos[i][0] < x_min:
+                x_min = self.mice_pos[i][0]
+            elif self.mice_pos[i][0] > x_max:
+                x_max = self.mice_pos[i][0]
+            if self.mice_pos[i][1] < y_min:
+                y_min = self.mice_pos[i][1]
+            elif self.mice_pos[i][1] > y_max:
+                y_max = self.mice_pos[i][1]
+        self.ax.set_xlim(x_min - 1, x_max + 1)
+        self.ax.set_ylim(y_min - 1, y_max + 1)
 
-# Function to update the plot in each animation frame
-def update(frame):
-    for i in range(mice_num):
-        lines[i].set_data(mice_pos[i][0][:frame + 1], mice_pos[i][1][:frame + 1])
-    return tuple(lines)
+    def animate(self):
+        animations = [FuncAnimation(self.fig, self.update, frames=self.num_frames, init_func=self.init, blit=True) for _ in range(self.mice_num)]
+        self.set_axis()
+        plt.legend()
+        plt.show()
 
-# Set appropriate axis limits based on the generated data
-x_min = np.min([np.min(mice_pos[i][0]) for i in range(mice_num)])
-x_max = np.max([np.max(mice_pos[i][0]) for i in range(mice_num)])
-y_min = np.min([np.min(mice_pos[i][1]) for i in range(mice_num)])
-y_max = np.max([np.max(mice_pos[i][1]) for i in range(mice_num)])
+# Create an instance of the Mice class
+mice_simulation = Mice(mice_num=3, num_frames=100)
 
-ax.set_xlim(x_min - 1, x_max + 1)
-ax.set_ylim(y_min - 1, y_max + 1)
-
-# Create animations
-animations = [FuncAnimation(fig, update, frames=num_frames, init_func=init, blit=True) for _ in range(mice_num)]
-
-# Show the animation
-plt.legend()
-plt.show()
+# Run the animation
+mice_simulation.animate()
