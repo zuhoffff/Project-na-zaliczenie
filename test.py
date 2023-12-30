a81f0cf1224907a2fe3ea8525f4ae7035090a39f
Nick <snicky5746@gmail.com>
2023-12-30 11:37:52 +0100

    remade data structures

--- test.py
+++ test.py
@@ -5,14 +5,20 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 from matplotlib.animation import FuncAnimation
 
-class AnimalSimulation:
-    def __init__(self, file_path, num_frames, color, label, max_distance=1.0):
-        self.num_frames = num_frames
-        self.fig, self.ax = plt.subplots()
-        self.lines = []
-        self.positions = self.retrieve(file_path)
-        self.circles = self.draw_circles(color)
-        self.max_distance = max_distance  # Include max_distance in the __init__ method
+class Creature:
+    def __init__(self, file_path, max_distance):
+        self.start_positions = self.retrieve(file_path) #is the separete start_positions actually required?
+        self.positions = []
+        self.max_distance = max_distance
+        self.num = len(self.start_positions)
+    def retrieve(self, file_path):
+        positions = []
+        with open(file_path, 'r') as file:
+            for line in file:
+                words = line.strip().split()
+                position = [float(words[0]), float(words[1])]
+                positions.append(position)
+        return positions
 
     def generate_next_point(self, x, y):
         angle = np.random.uniform(0, 2 * np.pi)
@@ -21,64 +27,78 @@ class AnimalSimulation:
         new_y = y + distance * np.sin(angle)
         return new_x, new_y
 
-    def generate_points(self, start_x, start_y):
-        x_data = [start_x]
-        y_data = [start_y]
-        for _ in range(1, self.num_frames):
-            x, y = self.generate_next_point(x_data[-1], y_data[-1])
-            x_data.append(x)
-            y_data.append(y)
-        return x_data, y_data
+class Mouse(Creature):
+    def __init__(self, file_path='mice.txt', max_distance=1):
+        super().__init__(file_path, max_distance)
 
-    def init(self):
-        self.lines = [self.ax.plot([], [], color='blue', label=self.label)[0] for _ in range(len(self.positions))]
-        return tuple(self.lines)
 
-    def update(self, frame):
-        for i in range(len(self.positions)):
-            new_point = self.generate_next_point(self.positions[i][0][-1], self.positions[i][1][-1])
-            self.positions[i][0].append(new_point[0])
-            self.positions[i][1].append(new_point[1])
-            self.lines[i].set_data(self.positions[i][0][:frame + 1], self.positions[i][1][:frame + 1])
-        return tuple(self.lines)
+class Cat(Creature):
+    def __init__(self, file_path='average_cats.txt', max_distance=10):
+        super().__init__(file_path, max_distance)
+
+
+class Simulation: #can be performed within creature class ?
+    def __init__(self, num_frames):
+        self.num_frames = num_frames
+        self.fig, self.ax = plt.subplots()
+        self.mice = Mouse()
+        self.cats = Cat()
 
     def set_axis(self):
-        x_min = np.min([np.min(animal[0]) for animal in self.positions])
-        x_max = np.max([np.max(animal[0]) for animal in self.positions])
-        y_min = np.min([np.min(animal[1]) for animal in self.positions])
-        y_max = np.max([np.max(animal[1]) for animal in self.positions])
-        self.ax.set_xlim(x_min - 1, x_max + 1)
-        self.ax.set_ylim(y_min - 1, y_max + 1)
+
+    def init(self):
+        self.mouse_lines = [self.ax.plot([], [], color='blue', label='Mice')[0] for _ in range(self.mice.num)]
+        self.cat_lines = [self.ax.plot([], [], color='blue', label='Cats')[0] for _ in range(self.cats.num)]
+        return tuple(self.mouse_lines), tuple(self.cat_lines)
+
+    def update(self, frame):
+        #just assign compete lists to lines instead?
+
+        for mouse in range(self.mice.num):
+            self.mouse_lines[mouse].set_data(
+                self.mice.positions[mouse][0][:frame+1],
+                self.mice.positions[mouse][1][:frame+1]
+            )
+        for cat in range():
+            self.cat_lines[cat].set_data(
+                self.cats.positions[cat][0][:frame + 1],
+                self.cats.positions[cat][1][:frame + 1]
+            )
+        return tuple(self.mouse_lines), tuple(self.cat_lines) #can a pair of tuples be returned and used then?
+
+
+    def check_interaction(self):
+        for frame in range (self.num_frames):
+            x_values = []
+            y_values = []
+            for mouse in self.mice.start_positions:
+                next_x, next_y = self.mice.generate_next_point(mouse[0], mouse[1])
+                x_values.append(next_x)
+                y_values.append(next_y)
+            self.mice.positions.append(x_values, y_values)
+
+
+            x_values = []
+            y_values = []
+            for cat in self.cats.start_positions:
+                next_x, next_y = self.cats.generate_next_point(cat[0], cat[1])
+                x_values.append(next_x)
+                y_values.append(next_y)
+            self.cats.positions.append(x_values, y_values)
 
     def animate(self):
         self.set_axis()
-        for circle in self.circles:
-            self.ax.add_patch(circle)
-        animations = [
-            FuncAnimation(self.fig, self.update, frames=self.num_frames, init_func=self.init, blit=True, interval=200)]
+        self.ax.legend()
+        mice_anim = FuncAnimation(self.fig, self.update[0], frames=self.num_frames, init_func=self.init, blit=True, interval=200)# can be replaced with one anim obj
+        cats_anim = FuncAnimation(self.fig, self.update[1], frames=self.num_frames, init_func=self.init, blit=True, interval=200)
         plt.legend()
         plt.show()
 
-    def retrieve(self, file_path):
-        animal_positions = []
-        with open(file_path, 'r') as file:
-            for line in file:
-                words = line.strip().split()
-                start_x, start_y = float(words[0]), float(words[1])
-                animal_positions.append(self.generate_points(start_x, start_y))
-        return animal_positions
+# Example usage:
 
-    def draw_circles(self, color):
-        label = 'Starts'
-        circles = [Circle((animal[0][0], animal[1][0]), radius=0.1, color='red', label=label) for animal in
-                   self.positions]
-        self.ax.legend([circles[0]], [label])
-        return circles
+def main():
+    simulation = Simulation(num_frames=100)
+    simulation.animate()
 
-# Example usage for Mice
-mice_simulation = AnimalSimulation(file_path='mice.txt', num_frames=100, color='blue', label='Mice')
-mice_simulation.animate()
-
-# Example usage for Cats
-cats_simulation = AnimalSimulation(file_path='cats.txt', num_frames=100, color='orange', label='Cats', max_distance=10.0)
-cats_simulation.animate()
+if __name__ == '__main__':
+    main()
