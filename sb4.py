616d44774f7aba613d1c27e49140e8ac8ed1f2ce
Nick <snicky5746@gmail.com>
2024-01-09 18:15:13 +0100

    kittens interaction implementation

--- sb4.py
+++ sb4.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 from matplotlib.animation import FuncAnimation
 
+#todo: craches if there is no creatures of any type
 class PlotManager:
     def __init__(self):
         self.fig, self.ax = plt.subplots()
@@ -67,23 +68,25 @@ class Creature:
 
     def init_plot(self):
         self.lines = [self.plot_manager.ax.plot([], [], color=self.color, label=self.label)[0] for _ in range(self.num)]
-        self.lines[0].set_label(self.label)
         return tuple(self.lines)
 
     def update(self, frame):
         arr_pos = np.array(self.positions)
         for i in range(self.num):
             self.lines[i].set_data(arr_pos[:frame + 1, i, 0], arr_pos[:frame + 1, i, 1])
+
         return tuple(self.lines)
 
-    def draw_circles(self, circle_label):
-        self.circles = [Circle((creature[0], creature[1]), radius=self.radius, color=self.color, label=None) for creature in
+    def draw_circles(self, label=None):
+        self.circles = [Circle((creature[0], creature[1]), radius=self.radius, color=self.color) for creature in
                         self.positions[0]]
-        self.circles[0].set_label(circle_label)
+        if label:
+            self.circles[0].set_label(label)
+
         return self.circles
 
     def animate(self, num_frames):
-        for circle in self.draw_circles('Starting Position'):
+        for circle in self.draw_circles(self.label):
             self.plot_manager.ax.add_patch(circle)
 
         self.anim = FuncAnimation(
@@ -94,6 +97,17 @@ class Mouse(Creature):
     def __init__(self, plot_manager, file_path='mice.txt', max_distance=2, color='blue', label='Mice', radius=0.15):
         super().__init__(plot_manager, file_path, max_distance, color, label, radius)
         self.flags = []
+
+    def interact(self, other):
+        for index, mouse in enumerate(self.positions[-1]):
+            for creature in other.positions[-1]:
+                mouse_arr = np.array(mouse)
+                creature_arr = np.array(creature)
+                distance = np.linalg.norm(mouse_arr - creature_arr)
+                if distance <= 10:
+                    self.flags.append(index)
+
+        return None
 
     def generate_points(self):
         next_positions = []
@@ -116,10 +130,31 @@ class Kitten(Creature):
     def __init__(self, plot_manager, file_path='kittens.txt', max_distance=5, color='purple', label='kittens',
                  radius=0.15):
         super().__init__(plot_manager, file_path, max_distance, color, label, radius)
+        self.flags = []
+
+    def interact(self, other):
+        for index, kitten in enumerate(self.positions[-1]):
+            kitten_start_arr = np.array(self.positions[0][index])
+            for other_index, creature in enumerate(other.positions[-1]):
+                kitten_arr = np.array(kitten)
+                creature_arr = np.array(creature)
+                distance = np.linalg.norm(kitten_arr - creature_arr)
+                #check the proximity of kitten and mouse
+                if distance <= 10:
+                    dist_to_home = np.linalg.norm(kitten_arr - kitten_start_arr)
+                    if dist_to_home > 50:
+                        self.flags.append(index)
+                    else:
+                        other.flags.append(other_index)
+
+
+        return None
 
     def generate_points(self):
         next_positions = []
         for i in range(self.num):
+            if i in self.flags:
+                next_positions.append(self.positions[0][i])
             start_x, start_y = self.positions[0][i]
             x = self.positions[-1][i][0]
             y = self.positions[-1][i][1]
@@ -147,14 +182,8 @@ class Simulation:
         self.num_frames = num_frames
         self.render_points()
 
-    def interact(self):
-        for index, mouse in enumerate(self.mice.positions[-1]):
-            for cat in self.cats.positions[-1]:
-                mouse_arr = np.array(mouse)
-                cat_arr = np.array(cat)
-                distance = np.linalg.norm(mouse_arr - cat_arr)
-                if distance <= 10:
-                    self.mice.flags.append(index)
+    def interact(self, creature1, creature2):
+        interaction_index = creature1.interact(creature2)
 
     def render_points(self):
         all_positions = [self.mice.positions, self.cats.positions, self.kittens.positions]
@@ -163,7 +192,10 @@ class Simulation:
             self.cats.generate_points()
             self.kittens.generate_points()
             self.mice.flags.clear()
-            self.interact()
+
+            # Interactions  todo: handle the priority of interactions
+            self.interact(self.mice, self.cats)
+            self.interact(self.kittens, self.mice) ##works correctly only in such order!
 
         self.plot_manager.set_axis(all_positions)
 
